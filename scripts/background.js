8b92cc5990f58fc7893151561ffbcbc51b4a08a6
Felix Irgendson <30195074+PhoenixClank@users.noreply.github.com>
2025-04-11 20:27:20 +0200

    fix broken infinite scroll in places that aren't the notification dropdown
    fixes #2

--- scripts/background.js
+++ scripts/background.js
@@ -7,18 +7,30 @@ function notificationMenuItemFilter(item) {
 browser.webRequest.onBeforeRequest.addListener(details => {
 	const filter = browser.webRequest.filterResponseData(details.requestId);
 	const decoder = new TextDecoder();
-	var str = "";
+	var str;
+	filter.onstart = () => {
+		str = "";
+	}
 	filter.ondata = event => {
 		str += decoder.decode(event.data);
 	};
 	filter.onstop = () => {
 		const response = JSON.parse(str);
-		if (Object.hasOwn(response.onResponseReceivedEndpoints[0], "openPopupAction")) for (const section of response.onResponseReceivedEndpoints[0].openPopupAction.popup.multiPageMenuRenderer.sections) {
-			const temp = section.multiPageMenuNotificationSectionRenderer.items.filter(notificationMenuItemFilter);
-			section.multiPageMenuNotificationSectionRenderer.items = temp;
-		} else {
-			const temp = response.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems.filter(notificationMenuItemFilter);
-			response.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems = temp;
+		if (Object.hasOwn(response, "onResponseReceivedEndpoints")) {
+			if (
+				Object.hasOwn(response.onResponseReceivedEndpoints[0], "openPopupAction")
+				&& Object.hasOwn(response.onResponseReceivedEndpoints[0].openPopupAction.popup, "multiPageMenuRenderer")
+				&& response.onResponseReceivedEndpoints[0].openPopupAction.popup.multiPageMenuRenderer.style == "MULTI_PAGE_MENU_STYLE_TYPE_NOTIFICATIONS"
+			) for (const section of response.onResponseReceivedEndpoints[0].openPopupAction.popup.multiPageMenuRenderer.sections) {
+				const temp = section.multiPageMenuNotificationSectionRenderer.items.filter(notificationMenuItemFilter);
+				section.multiPageMenuNotificationSectionRenderer.items = temp;
+			} else if (
+				Object.hasOwn(response.onResponseReceivedEndpoints[0], "appendContinuationItemsAction")
+				&& response.onResponseReceivedEndpoints[0].appendContinuationItemsAction.target == "CONTINUATION_TARGET_NOTIFICATION_MENU"
+			) {
+				const temp = response.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems.filter(notificationMenuItemFilter);
+				response.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems = temp;
+			}
 		}
 		filter.write(new TextEncoder().encode(JSON.stringify(response)));
 		filter.close();
